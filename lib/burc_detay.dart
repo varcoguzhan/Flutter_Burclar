70021a5909e2b387a83e8c95ecbc602c18859fb0
varcoguzhan <129661142+varcoguzhan@users.noreply.github.com>
2024-06-09 01:46:18 +0300

    proje bitiş

--- lib/burc_detay.dart
+++ lib/burc_detay.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_burclar/model/burc.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetay extends StatelessWidget {
+class BurcDetay extends StatefulWidget {
   final Burc secilenBurc;
   const BurcDetay({required this.secilenBurc, Key? key}) : super(key: key);
+
+  @override
+  State<BurcDetay> createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+  Color appBarRengi = Colors.transparent;
+  late PaletteGenerator _generator;
+  @override
+  void initState() {
+    super.initState();
+    appbarRenginiBul();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +27,12 @@ class BurcDetay extends StatelessWidget {
           SliverAppBar(
             expandedHeight: 250,
             pinned: true,
-            backgroundColor: Colors.pink,
+            backgroundColor: appBarRengi,
             flexibleSpace: FlexibleSpaceBar(
-              title: Text(secilenBurc.burcAdi + "Burcu ve özellikleri"),
+              title: Text(widget.secilenBurc.burcAdi + "Burcu ve özellikleri"),
               centerTitle: true,
-              background: Image.asset('images/' + secilenBurc.burcBuyukResim,
+              background: Image.asset(
+                  'images/' + widget.secilenBurc.burcBuyukResim,
                   fit: BoxFit.cover),
             ),
           ),
@@ -26,7 +41,7 @@ class BurcDetay extends StatelessWidget {
               margin: EdgeInsets.all(16),
               padding: EdgeInsets.all(8),
               child: SingleChildScrollView(
-                child: Text(secilenBurc.burcDetayi,
+                child: Text(widget.secilenBurc.burcDetayi,
                     style: Theme.of(context).textTheme.bodyLarge),
               ),
             ),
@@ -34,5 +49,15 @@ class BurcDetay extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void appbarRenginiBul() async {
+    _generator = await PaletteGenerator.fromImageProvider(
+        AssetImage('images/${widget.secilenBurc.burcBuyukResim}'));
+        appBarRengi=_generator.dominantColor!.color;
+        setState(() {
+          
+        });
+        print(appBarRengi);
   }
 }
